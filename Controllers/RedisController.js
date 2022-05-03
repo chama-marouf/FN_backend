d1320a6b0533bec2a3558651e88afb8baa9cf637
chama-marouf <ec_marouf@esi.dz>
2022-05-03 13:03:43 +0100

    adding delete  and update

--- Controllers/RedisController.js
+++ Controllers/RedisController.js
@@ -20,14 +20,14 @@ exports.get_all_locations = (req, res, next) => {
                 }
 
                 if (i == keys.length) {
-                    res.send({ users: return_dataset })
+                    res.send({ locations: return_dataset })
                 }
             })
         })
     })
 }
 
-exports.add_user = (req, res, next) => {
+exports.add_location = (req, res, next) => {
     // post Parameters
     let id = req.body.id
     let name = req.body.name
@@ -65,64 +65,66 @@ exports.add_user = (req, res, next) => {
     )
 }
 
-exports.delete_user = (req, res, next) => {
+exports.delete_location = (req, res, next) => {
     // find key associated with the id and deletes it
     client.del(req.params.id, (err, reply) => {
         if (err) {
-            console.log(err) // callback incase something goes wrong
+            console.log(err)
         }
 
-        console.log(reply) // log success message
-        res.send("User deleted successfully") // response back to the client
+        console.log(reply)
+        res.send("Location deleted successfully")
     })
 }
 
-exports.get_user = (req, res, next) => {
+exports.get_location = (req, res, next) => {
     // id from url Parameter
     let id = req.params.id
 
-    // get all values associated with the key as id
     client.hgetall(id, (err, obj) => {
         if (!obj) {
-            res.send("User does not exist") // if no user is associated with that id/key return this
+            res.send("Location does not exist")
         } else {
             obj.id = id
 
             res.send({
-                user: obj, // if user is found return details
+                location: obj,
             })
         }
     })
 }
 
-exports.update_user = (req, res, next) => {
-    // put Parameters
+exports.update_location = (req, res, next) => {
     let id = req.params.id
-    let first_name = req.body.first_name
-    let last_name = req.body.last_name
-    let email = req.body.email
-    let phone = req.body.phone
+    let name = req.body.name
+    let location = req.body.location
+    let chargers = req.body.chargers
+    let postalCode = req.body.postalCode
+    let lastUpdated = req.body.lastUpdated
+    let country = req.body.country
 
-    // make id the key and assign the id to the other Parameters
     client.hmset(
         id,
         [
-            "first_name",
-            first_name,
-            "last_name",
-            last_name,
-            "email",
-            email,
-            "phone",
-            phone,
+            "name",
+            name,
+            "location",
+            location,
+            "chargers",
+            chargers,
+            "postalCode",
+            postalCode,
+            "lastUpdated",
+            lastUpdated,
+            "country",
+            country,
         ],
         (err, reply) => {
             if (err) {
-                console.log(err) // callback to log errors
+                console.log(err)
             }
-
-            console.log(reply) // log success message
-            res.send("User updated successfully") // response to client
+            console.log(reply)
+            res.send("Location updated successfully")
         }
     )
 }
